31dc725a041ff42add14f81f937f6a949e904761
Phildo <Phildo211@gmail.com>
2014-03-01 19:44:23 -0600

    more blit setup

--- src/world_renderer.cpp
+++ src/world_renderer.cpp
@@ -3,36 +3,50 @@
 
 WorldRenderer::WorldRenderer(Graphics* g) : Renderer(g)
 {
-  gl_program_id = loadShader("/Users/pdougherty/Desktop/flat/src/shaders/w_shader.vs","/Users/pdougherty/Desktop/flat/src/shaders/w_shader.fs");
+  //Just 'constants' to better illustrate what the inputs that take ints are actually doing
+  GLuint attrib_0 = 0;
+  GLuint attrib_1 = 1;
 
-  //gen IDs
-  glGenVertexArrays(1, &gl_vert_array_id);
+  //Draw
+  gl_draw_program_id = loadShader("/Users/pdougherty/Desktop/flat/src/shaders/w_shader.vs","/Users/pdougherty/Desktop/flat/src/shaders/w_shader.fs");
+    //gen IDs
+  glGenVertexArrays(1, &gl_draw_vert_array_id);
+  glGenBuffers(1, &gl_draw_pos_buff_id);
+  glGenBuffers(1, &gl_draw_color_buff_id);
+  gl_draw_proj_mat_id  = glGetUniformLocation(gl_draw_program_id, "projMat");
+  gl_draw_view_mat_id  = glGetUniformLocation(gl_draw_program_id, "viewMat");
+  gl_draw_model_mat_id = glGetUniformLocation(gl_draw_program_id, "modelMat");
+  gl_draw_t_id         = glGetUniformLocation(gl_draw_program_id, "t");
+  t = 0;
+    //set up VAO
+  glUseProgram(gl_draw_program_id);
+  glBindVertexArray(gl_draw_vert_array_id);
+    //pos
+  glEnableVertexAttribArray(attrib_0);
+  glBindBuffer(GL_ARRAY_BUFFER, gl_draw_pos_buff_id);
+  glVertexAttribPointer(attrib_0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+    //color
+  glEnableVertexAttribArray(attrib_1);
+  glBindBuffer(GL_ARRAY_BUFFER, gl_draw_color_buff_id);
+  glVertexAttribPointer(attrib_1,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+
+  //Blit
+  gl_blit_program_id = loadShader("/Users/pdougherty/Desktop/flat/src/shaders/blit_shader.vs","/Users/pdougherty/Desktop/flat/src/shaders/blit_shader.fs");
+    //gen IDs
+  glGenVertexArrays(1, &gl_blit_vert_array_id);
+  glGenBuffers(1, &gl_blit_pos_buff_id);
+    //set up VAO
+  glUseProgram(gl_blit_program_id);
+  glBindVertexArray(gl_blit_vert_array_id);
+    //pos
+  glEnableVertexAttribArray(attrib_0);
+  glBindBuffer(GL_ARRAY_BUFFER, gl_blit_pos_buff_id);
+  glVertexAttribPointer(attrib_0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+
+  //set up FB
   glGenFramebuffers(1, &gl_fb_id);
   glGenTextures(1, &gl_fb_tex_buff_id);
   glGenRenderbuffers(1, &gl_fb_depth_buff_id);
-  glGenBuffers(1, &gl_pos_buff_id);
-  glGenBuffers(1, &gl_color_buff_id);
-  gl_proj_mat_id  = glGetUniformLocation(gl_program_id, "projMat");
-  gl_view_mat_id  = glGetUniformLocation(gl_program_id, "viewMat");
-  gl_model_mat_id = glGetUniformLocation(gl_program_id, "modelMat");
-  gl_t_id         = glGetUniformLocation(gl_program_id, "t");
-  t = 0;
-
-  //set up VAO
-  glUseProgram(gl_program_id);
-  glBindVertexArray(gl_vert_array_id);
-
-  GLuint attrib_0 = 0;
-  glEnableVertexAttribArray(attrib_0);
-  glBindBuffer(GL_ARRAY_BUFFER, gl_pos_buff_id);
-  glVertexAttribPointer(attrib_0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
-
-  GLuint attrib_1 = 1;
-  glEnableVertexAttribArray(attrib_1);
-  glBindBuffer(GL_ARRAY_BUFFER, gl_color_buff_id);
-  glVertexAttribPointer(attrib_1,3,GL_FLOAT,GL_FALSE,0,(void*)0);
-
-  //set up FB
   glBindFramebuffer(GL_FRAMEBUFFER, gl_fb_id);
 
   glBindTexture(GL_TEXTURE_2D, gl_fb_tex_buff_id);
@@ -51,25 +65,33 @@ WorldRenderer::WorldRenderer(Graphics* g) : Renderer(g)
 
 void WorldRenderer::loadVertData(const WorldComponent& rc) const
 {
-  glUseProgram(gl_program_id);
-  glBindVertexArray(gl_vert_array_id);
+  glUseProgram(gl_draw_program_id);
+  glBindVertexArray(gl_draw_vert_array_id);
 
-  glBindBuffer(GL_ARRAY_BUFFER, gl_pos_buff_id);
+  glBindBuffer(GL_ARRAY_BUFFER, gl_draw_pos_buff_id);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*rc.numVerts, (GLfloat *)rc.posData, GL_STATIC_DRAW);
-  glBindBuffer(GL_ARRAY_BUFFER, gl_color_buff_id);
+  glBindBuffer(GL_ARRAY_BUFFER, gl_draw_color_buff_id);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*rc.numVerts, (GLfloat *)rc.colorData, GL_STATIC_DRAW);
 }
 
 void WorldRenderer::render(const Camera* cam, const WorldComponent& rc) const
 {
-  glBindFramebuffer(GL_FRAMEBUFFER,0);
-  glUseProgram(gl_program_id);
-  glBindVertexArray(gl_vert_array_id);
-  glUniformMatrix4fv(gl_proj_mat_id,  1, GL_FALSE, &cam->projMat[0][0]);
-  glUniformMatrix4fv(gl_view_mat_id,  1, GL_FALSE, &cam->viewMat[0][0]);
-  glUniformMatrix4fv(gl_model_mat_id, 1, GL_FALSE, &rc.modelMat[0][0]);
-  glUniform1f(gl_t_id, t);
+  glBindFramebuffer(GL_FRAMEBUFFER,gl_fb_id);
+  glUseProgram(gl_draw_program_id);
+  glBindVertexArray(gl_draw_vert_array_id);
+  glUniformMatrix4fv(gl_draw_proj_mat_id,  1, GL_FALSE, &cam->projMat[0][0]);
+  glUniformMatrix4fv(gl_draw_view_mat_id,  1, GL_FALSE, &cam->viewMat[0][0]);
+  glUniformMatrix4fv(gl_draw_model_mat_id, 1, GL_FALSE, &rc.modelMat[0][0]);
+  glUniform1f(gl_draw_t_id, t);
   glDrawArrays(GL_TRIANGLES, 0, rc.numVerts);
+}
+
+void WorldRenderer::blit() const
+{
+  glBindFramebuffer(GL_FRAMEBUFFER,0);
+  glUseProgram(gl_blit_program_id);
+  glBindVertexArray(gl_blit_vert_array_id);
+  glDrawArrays(GL_TRIANGLES, 0, screen_quad.numVerts);
 }
 
 void WorldRenderer::update()
@@ -79,9 +101,13 @@ void WorldRenderer::update()
 
 WorldRenderer::~WorldRenderer()
 {
-  glDeleteVertexArrays(1, &gl_vert_array_id);
-  glDeleteBuffers(1, &gl_pos_buff_id);
-  glDeleteBuffers(1, &gl_color_buff_id);
-  glDeleteProgram(gl_program_id);
+  glDeleteVertexArrays(1, &gl_blit_vert_array_id);
+  glDeleteBuffers(1, &gl_blit_pos_buff_id);
+  glDeleteProgram(gl_blit_program_id);
+
+  glDeleteVertexArrays(1, &gl_draw_vert_array_id);
+  glDeleteBuffers(1, &gl_draw_pos_buff_id);
+  glDeleteBuffers(1, &gl_draw_color_buff_id);
+  glDeleteProgram(gl_draw_program_id);
 }
 
--- src/world_renderer.h
+++ src/world_renderer.h
@@ -9,29 +9,32 @@ class Camera;
 class WorldRenderer : public Renderer
 {
   private:
-    GLuint gl_program_id;
-
-    GLuint gl_vert_array_id;
-
     GLuint gl_fb_id;
     GLuint gl_fb_tex_buff_id;
     GLuint gl_fb_depth_buff_id;
 
-    GLuint gl_pos_buff_id;
-    GLuint gl_color_buff_id;
+    GLuint gl_draw_program_id;
+    GLuint gl_draw_vert_array_id;
+    GLuint gl_draw_pos_buff_id;
+    GLuint gl_draw_color_buff_id;
+    GLuint gl_draw_proj_mat_id;
+    GLuint gl_draw_view_mat_id;
+    GLuint gl_draw_model_mat_id;
+    GLuint gl_draw_t_id;
 
-    GLuint gl_proj_mat_id;
-    GLuint gl_view_mat_id;
-    GLuint gl_model_mat_id;
+    GLuint gl_blit_program_id;
+    GLuint gl_blit_vert_array_id;
+    GLuint gl_blit_pos_buff_id;
 
-    GLuint gl_t_id;
     float t;
+    WorldComponent screen_quad; //really just a quad- outside of entity_system
   public:
     WorldRenderer(Graphics* g);
     ~WorldRenderer();
 
     void loadVertData(const WorldComponent& rc) const;
     void render(const Camera* cam, const WorldComponent& rc) const;
+    void blit() const;
     void update();
 };
 
