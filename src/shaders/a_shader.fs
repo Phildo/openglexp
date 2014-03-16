38c7e6613dc0178b8eb27fb3ff3c230d0edea121
Phildo <Phildo211@gmail.com>
2014-03-16 17:42:41 -0500

    MUCH better shadowing with shadow2D!

--- src/shaders/a_shader.fs
+++ src/shaders/a_shader.fs
@@ -13,7 +13,7 @@ uniform sampler2D pos_tex;
 uniform sampler2D col_tex;
 uniform sampler2D norm_tex;
 uniform sampler2D dep_tex;
-uniform sampler2D shadow_tex;
+uniform sampler2DShadow shadow_tex;
 uniform sampler2D accum_tex; //the tex being drawn to
 
 void main()
@@ -24,10 +24,9 @@ void main()
   float d = sqrt(x*x + y*y + z*z);
 
   vec4 posInLightClip = lightProjMat * lightViewMat * texture(pos_tex, UV);
-  vec2 UVInLightTex = (((posInLightClip/posInLightClip.w).xy)+1)/2;
-  float ldep = 2.0 * (0.1*100) / ((100+0.1) - ((texture(shadow_tex, UVInLightTex).x*2)-1) * (100-0.1)); //supposedly the z of this fragment from the camera
+  vec3 shadowSTR = (((posInLightClip/posInLightClip.w).xyz)+1)/2;
 
-  if(UVInLightTex.x > 1 || UVInLightTex.y > 1 || UVInLightTex.x < 0 || UVInLightTex.y < 0 || abs(ldep-d) > 1) 
+  if(texture(shadow_tex, shadowSTR) == 0.0)
     color = vec3(0,0,0);
   else
     color = texture(accum_tex, UV).xyz+texture(col_tex, UV).xyz*(dot(lightPosVec-texture(pos_tex,UV).xyz,texture(norm_tex,UV).xyz)*0.5+0.5)*80/max(0.000001,d*d);
