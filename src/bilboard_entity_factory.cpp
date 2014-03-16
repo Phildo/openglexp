d5113d05d79f1d2bfa7230258f9e65747037526c
Phildo <Phildo211@gmail.com>
2014-03-16 15:40:50 -0500

    really cool pseudo shadowing

--- src/bilboard_entity_factory.cpp
+++ src/bilboard_entity_factory.cpp
@@ -19,17 +19,17 @@ void BilboardEntityFactory::produce(EntityPool* ep)
   Entity& e = ep->createEntity(ComponentSig_Geo);  
 
   GeoComponent& gc = ep->geoComponents[e.geoComponentIndex];
-  gc.pos[0] = glm::vec3(-100.0,-100.0,-10.0);
-  gc.pos[1] = glm::vec3(-100.0, 100.0,-10.0);
-  gc.pos[2] = glm::vec3( 100.0, 100.0,-10.0);
-  gc.pos[3] = glm::vec3(-100.0,-100.0,-10.0);
-  gc.pos[4] = glm::vec3( 100.0, 100.0,-10.0);
-  gc.pos[5] = glm::vec3( 100.0,-100.0,-10.0);
-  gc.color[0] = glm::vec3(0.2,0.2,0.2);
+  gc.pos[0] = glm::vec3(-15.0,-10.0,-8.0);
+  gc.pos[1] = glm::vec3(-15.0, 10.0,-8.0);
+  gc.pos[2] = glm::vec3( 15.0, 10.0,-8.0);
+  gc.pos[3] = glm::vec3(-15.0,-10.0,-8.0);
+  gc.pos[4] = glm::vec3( 15.0, 10.0,-8.0);
+  gc.pos[5] = glm::vec3( 15.0,-10.0,-8.0);
+  gc.color[0] = glm::vec3(0.8,0.2,0.2);
   gc.color[1] = glm::vec3(0.2,0.2,0.2);
-  gc.color[2] = glm::vec3(0.2,0.2,0.2);
-  gc.color[3] = glm::vec3(0.2,0.2,0.2);
-  gc.color[4] = glm::vec3(0.2,0.2,0.2);
+  gc.color[2] = glm::vec3(0.2,0.8,0.2);
+  gc.color[3] = glm::vec3(0.8,0.2,0.2);
+  gc.color[4] = glm::vec3(0.2,0.8,0.2);
   gc.color[5] = glm::vec3(0.2,0.2,0.2);
   gc.norm[0] = glm::vec3(0.0,0.0,1.0);
   gc.norm[1] = glm::vec3(0.0,0.0,1.0);
